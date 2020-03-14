acc921a3cc71eb3228e7e51bf5651ecbb9af10e1
francosansone <fracosansone11@gmail.com>
2020-03-14 19:05:45 -0300

    add user names

--- client.erl
+++ client.erl
@@ -3,54 +3,90 @@
 
 init(Host, Port) ->
     {ok, Socket} = gen_tcp:connect(Host, Port, [{active,false}, {packet,0}]),
-    spawn(?MODULE, controller, [Socket, self()]),
-    send_data(Socket, "").
+    Prompt = spawn(?MODULE, prompt, []),
+    spawn(?MODULE, controller, [Socket, self(), Prompt]),
+    send_data(Socket).
+
+init() -> init('localhost', 8000).
+
+init(Port) -> init('localhost', Port).
+
+prompt() ->
+    receive
+        {upd, Upd} ->
+            io:format("Game update ~p~n", [Upd]),
+            prompt()
+    end.
 
 
-send_data(Socket, Name) ->
-    {ok, Line} = read_stdin(),
-    io:format("~s~n", [Line]),
-    send(Socket, Name ++ "-" ++ Line),
+send_data(Socket) ->
+    Cmd = read_cmd(),
+    Args = read_args(),
+    Line = Cmd ++ (" " ++ Args),
+    send(Socket, Line),
     receive
         {userName, NewName} ->
             io:format("Update user name ~s~n", [NewName]),
             send_data(Socket, NewName);
         ok ->
-             send_data(Socket, Name);
+             send_data(Socket);
         _ ->
             exit(1)
     end.
 
+
+send_data(Socket, UserName) ->
+    Cmd = read_cmd(),
+    Args = read_args(),
+    Line = Cmd ++ ( " " ++ (UserName ++ (" " ++ Args))),
+    send(Socket, Line),
+    receive
+        ok ->
+            io:format("ok~n"),
+             send_data(Socket, UserName);
+        _ ->
+            io:format("no ok~n"),
+            send_data(Socket, UserName)
+    end.
+
 read_stdin() ->
-    io:format("Enter command: "),
     case io:get_line("") of
         eof ->
             init:stop();
         "\n" ->
-            io:format("Bad value~n"),
-            read_stdin();
+            {ok, ""};
         Line->
             Res = string:sub_string(Line, 1, string:len(Line) - 1),
             % aca una funcion filter, para filtrar comandos
             {ok, Res}
     end.
 
+read_cmd() ->
+    io:format("Enter command: "),
+    {ok, Cmd} = read_stdin(),
+    Cmd.
+
+read_args() ->
+    io:format("Enter arguments: "),
+    {ok, Args} = read_stdin(),
+    Args.
 
 send(Socket, Data) ->
+    io:format("sending ~s~n", [Data]),
     gen_tcp:send(Socket, Data).
 
 
-controller(Socket, Pid) ->
+controller(Socket, Pid, Prompt) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Command} ->
             case Command of
                 "OK CON " ++ Name ->
-                    io:format("new name!"),
-                    Pid!{userName, Name};
+                    Pid!{userName, Name},
+                    Prompt!{upd, "update bro~n"};
                 _ ->
                     Pid!ok
             end,
-            controller(Socket, Pid);
+            controller(Socket, Pid, Prompt);
         {error, Reason} ->
             io:format("Error ~p~n", [Reason]),
             gen_tcp:close(Socket),
--- server.erl
+++ server.erl
@@ -4,36 +4,86 @@
 server(Port) ->
     {ok, ListenSocket} = gen_tcp:listen(Port, [{active,false}, {packet,0}]),
     io:format("Listening in port ~p~n", [Port]),
+    UserNamesPid = spawn(?MODULE, usernames, [[]]),
+    global:register_name("name_list", UserNamesPid),
     dispatcher(ListenSocket, Port).
+
 
 dispatcher(ListenSocket, Port) ->
     {ok, Socket} = gen_tcp:accept(ListenSocket),
     spawn(?MODULE, psocket, [Socket]),
     dispatcher(ListenSocket, Port).
 
+
 psocket(Socket) ->
     io:format("psocket, ~p~n", [Socket]),
     case gen_tcp:recv(Socket, 0) of
-        {ok, Command} ->
-            spawn(?MODULE, pcommand, [self(), Command]),
+        {ok, "CON " ++ Name} ->
+            io:format("Recibe nombre ~s~n", [Name]),
+            UserNamesPid = global:whereis_name("name_list"),
+            UserNamesPid!{newName, Name, self()},
             receive
-                {ok, Result} ->
-                    gen_tcp:send(Socket, Result);
-                _ ->
-                    io:format("Error in psocket")
-            end,
-            psocket(Socket);
+                ok ->
+                    io:format("ok~n"),
+                    gen_tcp:send(Socket, "OK " ++ ("CON " ++ Name)),
+                    psocket(Socket, Name);
+                error ->
+                    io:format("error~n"),
+                    gen_tcp:send(Socket, "ERROR " ++ ("CON " ++ Name)),
+                    psocket(Socket)
+            end;
         {error, Reason} ->
             io:format("closed ~p~n",[Reason]),
             gen_tcp:close(Socket)
     end.
 
-pcommand(Parent, Command) ->
+
+usernames(UsernameList) ->
+    receive
+        {newName, Name, Pid} ->
+            % agregar aca un chequeo a los demas servidores que esten corriendo
+            case lists:member(Name, UsernameList) of
+                 true ->
+                    Pid!error,
+                    usernames(UsernameList);
+                false ->
+                    Pid!ok,
+                    io:format("new name!~s~n", [Name]),
+                    usernames(UsernameList ++ [Name])
+            end
+    end.
+
+
+
+psocket(Socket, UserName) ->
+    io:format("psocket, ~p~n", [Socket]),
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Command} ->
+            spawn(?MODULE, pcommand, [self(), Command, UserName]),
+            receive
+                {ok, Result} ->
+                    gen_tcp:send(Socket, Result);
+                _ ->
+                    io:format("Error in pcommand")
+            end,
+            psocket(Socket, UserName);
+        {error, Reason} ->
+            io:format("closed ~p~n",[Reason]),
+            gen_tcp:close(Socket)
+    end.
+
+pcommand(Parent, Command, UserName) ->
     io:format("pcommand ~p~n", [Command]),
     case Command of
-        "-CON " ++ Name ->
-            io:format("CON ~s~n", [Name]),
-            Parent!{ok, "OK CON " ++ Name};
+        "LSG " ++ Name ->
+            io:format("LSG ~s ~s~n", [Name, UserName]),
+            Parent!{ok, "OK LSG " ++ Name};
+        "NEW " ++ Name ->
+            io:format("~s: ~s~n", [Name, Command]),
+            Parent!{ok, "OK " ++ Command};
+        "ACC " ++ Rest ->
+            io:format("~s: ~s~n", [UserName, Command]),
+            Parent!{ok, "OK " ++ Command};
         _ ->
             io:format("Unexpected~n"),
             Parent!{ok, "ERROR No implementado"}
